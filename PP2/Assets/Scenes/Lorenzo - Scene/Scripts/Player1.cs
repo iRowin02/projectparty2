d959c81653dc5b93546c87a20f6b824b5c976a27
LorenzoGreidanus <49709495+LorenzoGreidanus@users.noreply.github.com>
2019-05-27 15:15:31 +0200

    LorenzoScene

---
+++ PP2/Assets/Scenes/Lorenzo - Scene/Scripts/Player1.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    public float speed;
+    public Transform target;
+    private int wavePointIndex = 0;
+    public int waypointsToGo;
+    public GameObject playerManager;
+
+    void Start()
+    {
+        //Start
+        speed = 0f;
+        target = Waypoints.waypoints[0];
+        waypointsToGo = GameObject.FindGameObjectWithTag("PlayerManager").GetComponent<PionMovement>().randomNumber;
+        playerManager = GameObject.FindGameObjectWithTag("PlayerManager");
+    }
+
+    void Update()
+    {
+        //Movement Waypoints
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (waypointsToGo == 0)
+        {
+            speed = 0f;
+        }
+
+        if (waypointsToGo != 0)
+        {
+            if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+            {
+                GetNextWaypoint();
+                waypointsToGo--;
+                playerManager.GetComponent<PionMovement>().randomNumber--;
+            }
+            if (wavePointIndex == 13)
+            {
+                wavePointIndex = -1;
+            }
+            if (waypointsToGo <= 0)
+            {
+                GameObject.FindGameObjectWithTag("PlayerManager").GetComponent<PionMovement>().Check();
+            }
+        }
+        void GetNextWaypoint()
+        {
+            wavePointIndex++;
+            target = Waypoints.waypoints[wavePointIndex];
+        }
+    }
+}
---
+++ PP2/Assets/Scenes/Lorenzo - Scene/Scripts/RandomScene.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RandomScene : MonoBehaviour
+{
+    public int minScene;
+    public int maxScene;
+
+    public void RandomSceneLoader()
+    {       
+        int scene = Random.Range(minScene, maxScene);
+        SceneManager.LoadScene(scene);
+    } 
+}
