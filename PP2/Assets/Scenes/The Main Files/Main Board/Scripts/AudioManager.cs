349b193f53e072252e821366a1142f4e12f916e3
wtfRowin <r.s.salomons@gmail.com>
2019-05-28 11:17:28 +0200

    wat music shit soortvan
    Is nog niet klaar maar zit er wel in, het moet alleen niet stoppen met spelen nadat je van scene wisselt.

---
+++ PP2/Assets/Scenes/The Main Files/Main Board/Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip buttonSound;
+    public AudioSource musicSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SoundOnClick(){
+        musicSource.clip = buttonSound;
+        musicSource.Play();
+    }
+}
